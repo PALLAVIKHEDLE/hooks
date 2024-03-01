1ddf8e5c4bc3de0ecb6a06505ce88c0c17f66c35
PallaviKhedle <pallavi8khedle@gmail.com>
2024-03-01 12:41:47 -0800

    useEffect changes

--- src/App.js
+++ src/App.js
@@ -1,65 +1,8 @@
-import React from 'react';
-
-import {ThemeProvider} from './useContext/ThemeContext'
-import FunctionComp from './useContext/FunctionComponent';
-
-import MemoComp from './useMemo/useMemoComp';
-
-import CallbackDemo from './useCallback/CallBackDemo';
-
-import Counter from './useReducer/Counter';
-import ToDoComp from './useReducer/ToDoComp';
-
-import UseTransisitionComp from './useTransition/useTransition';
-
-import UseDeferredComp from './useDeferred/useDeferred';
-
-function App() {
-  return (
-    <div style={{marginLeft: '0.5rem'}}>
-    <ThemeProvider>
-      <FunctionComp/>
-    </ThemeProvider>
-    <hr/>
-    <p>useMemo Demo, Above and Below Component are separate and don't share any state between them </p>
-    <MemoComp/>
-
-    <hr/>
-    <p>useCallBack Demo, Above and Below Component are separate and don't share any state between them </p>
-    <CallbackDemo/>
-
-    <hr/>
-    <p>useReducer Demo, Above and Below Component are separate and don't share any state between them </p>
-    <Counter/>
-    <ToDoComp/>
-
-    <hr/>
-    <p>useTransistion Demo, Above and Below Component are separate and don't share any state between them </p>
-    <UseTransisitionComp/>
-
-    <hr/>
-    <p>useDeferred Demo, Above and Below Component are separate and don't share any state between them </p>
-    <UseDeferredComp/>
-    </div>
-  );
-}
-
-export default App;
+import  Test from './useEffect/index'
 
 
-// const [darkTheme, setDarkTheme] = useState(false);
 
-//   function toggleDarkTheme() {
-//     setDarkTheme(!darkTheme);
-//   }
+export default function App() {
   
-//   return (
-//     <ThemeContext.Provider value= {darkTheme}>
-//     <div className="App">
-//       <button style = {{margin: '1rem', padding: '0.5rem'}} onClick={toggleDarkTheme} >Toggle Theme</button>
-//       <ClassBasedComp/>
-//       <FunctionComp/>
-//     </div>
-//     </ThemeContext.Provider>
-//   );
-// }
+return <Test/>
+}
